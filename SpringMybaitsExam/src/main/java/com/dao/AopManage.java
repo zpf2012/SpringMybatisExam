13ad54addb6c6d7bf19a3d232592e30f451cdc2f
pengfei.zhang@hand-china.com <pengfei.zhang@hand-china.com>
2016-08-15 22:51:49 +0800

    xiugai

--- SpringMybaitsExam/src/main/java/com/dao/AopManage.java
+++ SpringMybaitsExam/src/main/java/com/dao/AopManage.java
@@ -1,4 +1,4 @@
-package com.aop;
+package com.dao;
 
 import java.util.Arrays;
 import java.util.List;
@@ -7,21 +7,26 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+import com.hand.Customer;
 
+
+@Order(1)
 @Aspect
 @Component
 public class AopManage {
 	
-	@Before(value = "execution(* com.dao.Customerdao.*(..))")
+	@Before("execution(public void insetCustomer(..))")
 	public void BeforeInsertFilmEvent(JoinPoint joinPoint){
+		System.out.println("Before Insert Customer Data:");
 		System.out.println("已保存的数据如下：");
 		List<Object> customers = Arrays.asList(joinPoint.getArgs());
 		System.out.println(customers);
 	}
 	
-	@After(value = "execution(* com.dao.Customerdao.*(..))")
+	@After("execution(public void insetCustomer(..))")
 	public void AfterInsertFilmEvent(JoinPoint joinPoint){
 		System.out.println("After Insert Customer Data");
 	}
